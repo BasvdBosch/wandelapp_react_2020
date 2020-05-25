d28c862f1cfac58906da1c831a6cab5bf2ec5d15
Your Name <you@example.com>
2020-05-25 14:50:23 +0200

    update

--- src/modules/keytext_AANPASSEN.js
+++ src/modules/keytext_AANPASSEN.js
@@ -1,4 +1,4 @@
 // https://docs.mapbox.com/help/how-mapbox-works/access-tokens/
 export default function mapkey() {
-  return 'HIER JOUW KEY VAN MAPBOX GL';
+  return 'pk.eyJ1Ijoiam9iMTEyIiwiYSI6ImNrYW1objM3aTA2eW8yeXBmdWQwbmN6MmQifQ.nrT-fbkjx_dVQkf81ZDLMg';
 }
