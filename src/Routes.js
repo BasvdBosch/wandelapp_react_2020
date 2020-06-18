e23ccd5b80eca98cc80a8c9de184b61e01515b8b
Your Name <you@example.com>
2020-06-18 13:46:31 +0200

    UPDATE

--- src/Routes.js
+++ src/Routes.js
@@ -2,69 +2,77 @@ import React, {Component} from 'react';
 import {getroutesjson} from './rest_routes';
 
 class Routes extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      routes: []
-    };
-  }
-
-  getRoutes() {
-    //Get routes from server
-    const cuid = 'test';
-    const remoteServer = 'https://wandelappbackend-v5.herokuapp.com';
-    getroutesjson(remoteServer + '/routes?cuid=' + cuid)
-      .then(
-        (routesjson) => {
-          this.setState({routes: routesjson});
-        },
-        (reason) => {
-          // Error retreiving routes!
-          console.log(reason);
-        }
-      )
-      .catch(
-        (e) => {
-          console.log(e);
-        }
-      )
-    ;
-  }
-
-  componentDidMount() {
-    this.getRoutes();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.routes === []) {
-      this.getRoutes();
+    constructor(props) {
+        super(props);
+        this.state = {
+            routes: []
+        };
     }
-  }
 
-  componentWillReceiveProps(props) {
-    if (props.refresh) {
-      this.getRoutes();
+    getRoutes() {
+        //Get routes from server
+        const cuid = 'test';
+        const remoteServer = 'https://wandelappbackend-v5.herokuapp.com';
+        getroutesjson(remoteServer + '/routes?cuid=' + cuid)
+            .then(
+                (routesjson) => {
+                    this.setState({routes: routesjson});
+                },
+                (reason) => {
+                    // Error retreiving routes!
+                    console.log(reason);
+                }
+            )
+            .catch(
+                (e) => {
+                    console.log(e);
+                }
+            )
+        ;
     }
-  }
 
-  selectRoute = (data) => {
-    this.props.onRouteSelect(data.json);
-  }
+    delRoutes() {
 
-  render() {
-    const style = {
-      flex: '1 1 0'
-    };
-    return (
-      <div style={style}>
-        <ul>
-          {this.state.routes.map(route =>
-            <li onClick={this.selectRoute.bind(this, route.data)} key={route.data._id}>{route.data.json.features[0].properties.name}</li>
-          )}
-        </ul>
-      </div>
-    );
-  }
+    }
+
+    componentDidMount() {
+        this.getRoutes();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.routes === []) {
+            this.getRoutes();
+        }
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.refresh) {
+            this.getRoutes();
+        }
+    }
+
+    selectRoute = (data) => {
+        this.props.onRouteSelect(data.json);
+    }
+
+    render() {
+        const style = {
+            flex: '1 1 0'
+        };
+        return (
+            <div style={style}>
+                <ul>
+                    {this.state.routes.map(route =>
+                        <li onClick={this.selectRoute.bind(this, route.data)}
+                            key={route.data._id}>{route.data.json.features[0].properties.name}
+                            <button onClick={console.log(route.data.json.features[0].properties.name)}>Delete</button>
+                        </li>
+                    )}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Routes;
+// route.data._id
