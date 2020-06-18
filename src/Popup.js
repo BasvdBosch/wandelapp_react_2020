20881d246e8bd8c9dfcd43c6b34c079c6b91f5de
LukeGeuze <luke.geuze@ictmbo.nl>
2020-06-18 14:08:36 +0200

    popup af

--- src/Popup.js
+++ src/Popup.js
@@ -1,19 +1,50 @@
 import React, {Component} from 'react';
 import './App.js';
+let popupStyle = {
+    width: '500px',
+    maxWidth: '100%',
+    margin: '0 auto',
+    position: 'fixed',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%,-50%)',
+    zIndex: '999',
+    backgroundColor: '#eee',
+    padding: '10px 20px 40px',
+    borderRadius: '8px',
+    display: 'flex',
+    flexDirection: 'column'
+};
 
+let popupStyleClose = {
+    marginBottom: '15px',
+    padding: '3px 8px',
+    cursor: 'pointer',
+    borderRadius: '50%',
+    border: 'none',
+    width: '30px',
+    height: '30px',
+    fontWeight: 'bold',
+    alignSelf: 'flex-end'
+};
 
 class Popup extends Component {
-    closePopup;
     render() {
-        return (
-            <div className='popup'>
-                <div className='popup_inner'>
-                    <h1>{this.props.text}</h1>
-                    <button onClick={this.props.closePopup}>sluiten</button>
-                </div>
+        let popup = (
+            <div style={popupStyle}>
+            <button style={popupStyleClose} onClick={this.props.onClose}>x</button>
+
+            <div>{this.props.children}</div>
             </div>
         );
+        if (! this.props.isOpen){
+            popup = null ;
+        }
+        return(
+            <div>
+                {popup}
+            </div>
+        )
     }
-}
-
+    }
 export default Popup;
